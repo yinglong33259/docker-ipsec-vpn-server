eaef61c390a98db6f93352450d56bf871aab3d5c
DESKTOP-XIAOSHA\xiaoshan <huangxs@dingcloud.com>
2020-03-09 14:18:46 +0800

    update

--- run.sh
+++ run.sh
@@ -29,6 +29,10 @@ ip link delete dummy0 >/dev/null 2>&1
 
 mkdir -p /opt/src
 
+echo "$VPN_DEFAULT_USER"
+ss=$(cat /opt/src/VPN_DEFAULT_USER)
+echo $ss
+
 # Remove whitespace and quotes around VPN variables, if any
 VPN_IPSEC_PSK=$(nospaces "$VPN_DEFAULT_PSK")
 VPN_IPSEC_PSK=$(noquotes "$VPN_DEFAULT_PSK")
